b14a95e25dbf5c88cb11b5c03817017b19432363
Baichuan Li <ilovegithub@patrickli.mozmail.com>
2024-10-23 00:33:46 -0400

    runner ready

---
+++ run_walker.py
@@ -0,0 +1,15 @@
+import subprocess
+import os
+import json
+
+
+result = []
+for i in range(10):
+    new_env = os.environ.copy()
+    new_env["SEED"] = str(i)
+    subprocess.run(["jac", "run", "wikiwalker.jac"], env=new_env)
+    with open("single_result.json", "r") as f:
+        result.append(json.load(f))
+
+with open("result.json", "w") as f:
+    json.dump(result, f)
